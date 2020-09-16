9edd1afddd38eab7a6650b3ae023406e3ca0c943
Hao-Ting Li @kurisu <remorse@csie.io>
2020-09-17 05:44:54 +0800

    fix: filename doesn't match between training stage and testing stage error

--- libs/dataset.py
+++ libs/dataset.py
@@ -6,9 +6,6 @@ from torch.utils.data import Dataset
 
 
 class PlantSeedlingsDataset(Dataset):
-    CATEGORY_TO_LABEL = {}
-    LABEL_TO_CATEGORY = {}
-
     def __init__(self, root, stage='train', transforms=None):
         """
         :param root: path to the dataset, where contains `train`, `test` and `sample_submission.csv`
@@ -20,6 +17,7 @@ class PlantSeedlingsDataset(Dataset):
         self._root_stage = Path(root) / stage
         self._stage = stage
         self._transforms = transforms
+        self._converter = LabelNameConverter(root)
         if stage == 'train':
             self._filenames, self._labels = self._extract_filename_label()
         else:
@@ -28,19 +26,14 @@ class PlantSeedlingsDataset(Dataset):
     def _extract_filename_label(self):
         filenames = []
         labels = []
-        label_index = 0
 
         # root_stage/{Black-grass,Charlock,...}/{hash_id}.png
         # category_folder.name: Black-grass, Charlock, ...
         for category_folder in self._root_stage.iterdir():
             filenames_in_category = [filename for filename in category_folder.iterdir()]
             filenames += filenames_in_category
-
-            self.CATEGORY_TO_LABEL[category_folder.name] = label_index
-            self.LABEL_TO_CATEGORY[label_index] = category_folder.name
-
+            label_index = self._converter[category_folder.name]
             labels += [label_index] * len(filenames_in_category)
-            label_index += 1
 
         return filenames, labels
 

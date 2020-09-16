8a73b1c78f52b54730d8c66c3a7b8835ec95f7f2
Hao-Ting Li @kurisu <remorse@csie.io>
2020-09-17 07:06:30 +0800

    feat: add data augmentation fix: tune hyperparameters

--- train.py
+++ train.py
@@ -12,21 +12,28 @@ from libs.visualization import LossPresenter, LossVisualizer
 
 DATA_ROOT = os.environ['DATA_ROOT']
 DEVICE = 'cpu'
+# DEVICE = 'cuda'
+
 # optimizer
-LEARNING_RATE = 0.001
+LEARNING_RATE = 0.0001
 WEIGHT_DECAY = 0.01
 # data loader
-BATCH_SIZE = 16
+BATCH_SIZE = 32
 NUM_WORKERS = 8
 # training
 NUM_EPOCHS = 100
 SAVE_INTERNAL = 5
-SAVE_ROOT = Path('params')
-LOG_ROOT = Path('logs')
+EXP_NAME = 'exp01'
+SAVE_ROOT = Path('params') / EXP_NAME
+SAVE_ROOT.mkdir(parents=True, exist_ok=True)
+LOG_ROOT = Path('logs') / EXP_NAME
+LOG_ROOT.mkdir(parents=True, exist_ok=True)
 
 
 def build_data_loader():
     transform = transforms.Compose([
+        transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
+        transforms.RandomRotation(90),
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
